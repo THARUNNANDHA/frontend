d98e35b2f04e9737e244d545c50d7a907483a6e4
THARUNNANDH <ntharun832@gmail.com>
2024-06-13 18:12:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/index.css';
 import Fetchuserdata from './hooks/Fetchuserdata';
 import Nav from "./components/Navbar"
@@ -8,47 +8,52 @@ import Part4 from './Part4';
 // import Signup from './Signup'
 import Products_display from './Products_display';
 import Signup from './Signup';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Login from './Login';
 import Forgotpassword from './Forgotpassword'
 import Homepage from './Homepage';
+import axios from 'axios';
 export default function App() {
+    const Usertracker = () => {
+        const location = useLocation();
+
+        useEffect(() => {
+            const trackUser = async () => {
+                try {
+                    const resp = await axios.post("/user_tracker", { location: location.pathname, timestamp: new Date() });
+                    console.log(resp);
+                } catch (error) {
+                    console.error("error", error);
+                }
+            };
+            trackUser();
+        }, [location]);
+
+        return null;
+    };
+
     const [sharedValue, setSharedValue] = useState({
-        name:"",
-        email:"",
-        password:""
+        name: "",
+        email: "",
+        password: ""
     });
     return (
         <BrowserRouter>
+            <Usertracker />
             <Routes>
                 <Route path="/" element={<Nav sharedValue={sharedValue} />}>
                     <Route index element={
                         <div>
-                            <Homepage setSharedValue={setSharedValue}/>
+                            <Homepage setSharedValue={setSharedValue} />
                         </div>
-                    }/>
+                    } />
                     <Route path="product" element={<Fetchuserdata api="/product_item_data" func={Products_display} />} />
-                    <Route path="signup" element ={<Fetchuserdata api="/user_data" func={Signup} />} />  
-                    <Route path="login" element ={<Fetchuserdata api="/user_data" func={Login} />} />
-                    <Route path="forgot_password" element ={<Fetchuserdata api="/user_data" func={Forgotpassword} />} />  
+                    <Route path="signup" element={<Fetchuserdata api="/user_data" func={Signup} />} />
+                    <Route path="login" element={<Fetchuserdata api="/user_data" func={Login} />} />
+                    <Route path="forgot_password" element={<Fetchuserdata api="/user_data" func={Forgotpassword} />} />
                 </Route>
             </Routes>
         </BrowserRouter>
     );
 }
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     {/* <Nav /> */}
-//     <Fetchuserdata 
-//       api='/data'
-//       func={Products_display} 
-//     />
-//     {/* <Nav />
-//     <Carousel />
-//     <Belowcarasule/>
-//     <Part4 /> */}
-    
-//   </React.StrictMode>
-// );
 
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,101 +1,13 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import './css/Signup.css'
-// function Validate_user({ token }) {
-//     useEffect(() => {
-//         const fetchData = () => {
-//             fetch(`/validate_user/${token}`)
-//                 .then(response => response.json())
-//                 .then(data => {
-//                     console.log(data);
-//                 })
-//                 .catch(error => console.error("Error fetching:", error));
-//         };
-//         fetchData();
-//         const interval = setInterval(fetchData, 2000);
-//         return () => clearInterval(interval);
-//     }, [token]);
-// }
-
-// function Signup({users}) {
-//     console.log(users);
-//     const [formData, setFormData] = useState({
-//         name: '',
-//         email: '',
-//         password:''
-//     });
-
-
-//     // console.log(formData);
-//     const handleChange = (e) => {
-//         setFormData({
-//             ...formData,
-//             [e.target.name]: e.target.value
-//         });
-//     };
-
-//     const handleSubmit = (e) => {
-
-//         e.preventDefault();
-//         // const{email} = formData
-//         const exist_email = users.some(user => user.email === formData.email)
-//         const exist_name = users.some(user => user.name === formData.name)
-//         console.log((formData.password).length)
-//         if(exist_email){
-//             alert('Email already exists');
-//         }
-//         else if(exist_name){
-//             console.log(formData.name)
-//             alert('Name already exists');
-//         }
-//         else{
-//             axios.post('/api/data', formData)
-//             .then(response => {
-//                 console.log(response.data);
-//                 // Handle response if needed
-//             })
-//             .catch(error => {
-//                 console.error('Error sending data:', error);
-//             });
-//             alert('Date inserted successfully');
-//         }
-
-//     };
-
-
-//     return (
-//         <div className='Signup_outer'>
-//                 {/* {users.map(user=>(
-//                     <h1>{user.username}</h1>
-//                 ))} */}
-//                 <h1>Sign_up</h1>
-//                 <form onSubmit={handleSubmit}>
-//                     <label>Name:</label>
-//                     <input type="text" name="name" value={formData.name} onChange={handleChange} />
-//                     <label>Email:</label>
-//                     <input type="email" name="email" value={formData.email} onChange={handleChange} />
-//                     <label>Password</label>
-//                     <input type="password" name="password" value={formData.password} onChange={handleChange} />
-//                     <button type="submit">Submit</button>
-//                 </form>
-//         </div>
-//     );
-// }
-
-// export default Signup;
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './css/Signup.css';
 import { useNavigate } from 'react-router-dom';
 
 
-function Signup({ users }) {
+function Signup() {
 
     const [validated_state, setvalidated_state] = useState([])
     const navigate = useNavigate();
-    console.log("users", users);
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -111,46 +23,22 @@ function Signup({ users }) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const exist_email = users.some(user => user.email === formData.email);
-        const exist_name = users.some(user => user.name === formData.name);
-        if (exist_email) {
-            alert('Email already exists');
-        } else if (exist_name) {
-            alert('Name already exists');
-        } else {
-            axios.post('/api/data', formData)
-                .then(response => {
-                    console.log(response.data);
-                    // Handle response if needed
-                    alert('Data inserted successfully');
-                })
-                .catch(error => {
-                    console.error('Error sending data:', error);
-                });
-        }
-        const fetchData = () => {
-            console.log("formdata", formData.email)
-            fetch(`/validated_data`)
-
-                .then(response => response.json())
-                .then(data => {
-                    console.log("validated data", data);
-                    if (data) {
-                        var exist = data.find(item => item.email === formData.email)
-                        if (exist) {
-                            console.log("inside fetch", exist)
-                            navigate('/');
-                            clearInterval(interval);
-                        }
-                    }
-                })
-                .catch(error => console.error("Error fetching:", error));
-        };
-        const interval = setInterval(fetchData, 6000);
-        return () => clearInterval(interval);
-    };
-
-
+        axios.post('/signin', formData)
+            .then(response => {
+                if (response.data.fail) {
+                    console.log(response.data.fail);
+                    alert(response.data.fail);
+                }
+                else if (response.data.success) {
+                    console.log(response.data.success);
+                    alert(response.data.success);
+                    navigate('/login');
+                }
+            })
+            .catch(error => {
+                console.error('Error sending data:', error);
+            });
+    }
 
 
     return (
